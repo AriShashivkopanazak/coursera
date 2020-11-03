30c006640d7edd8f579f92e9d7be284f388898b6
AriShashivkopanazak <ari.shashivkopanazak@gmail.com>
2020-11-03 13:18:28 -0800

    week2 on its way

---
+++ go/src/main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+//fibonacci sequence implementing Elucidian Algorithm
+/*
+given a and b
+
+4, 8
+
+4 % 8 = 4
+new a and b will be
+
+8, 4
+
+4 % 8
+*/
+
+func main() {
+
+	//var a int64
+	var b int64
+
+	x := 8 % 4
+	fmt.Println(x)
+	for b != 0 {
+
+	}
+}
---
+++ submissions/Fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+  private static long calc_fib(int n) {
+
+      long[] fibArray = new long[46];
+      fibArray[0] = 0;
+      fibArray[1] = 1;
+      fibArray[2] = 1;
+
+      int index = 2;
+
+      if (n > 2) {
+          while (index < n + 1) {
+              fibArray[index] = fibArray[index - 2] + fibArray[index - 1];
+              index++;
+          }
+      }
+      return fibArray[n];
+  }
+
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+
+    System.out.println(calc_fib(n));
+  }
+}
